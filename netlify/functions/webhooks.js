dc5a7e168a486a680076bb30727684c6082513bf
Ryan Garant <rgarant@tenable.com>
2022-08-17 20:27:14 -0500

    test

--- netlify/functions/webhooks.js
+++ netlify/functions/webhooks.js
@@ -1,9 +1,17 @@
-const {
-  createLambdaFunction,
-  createProbot,
-} = require('@probot/adapter-aws-lambda-serverless')
-const app = require('../../lib')
+// const {
+//   createLambdaFunction,
+//   createProbot,
+// } = require('@probot/adapter-aws-lambda-serverless')
+// const app = require('../../lib')
 
-module.exports.handler = createLambdaFunction(app, {
-  probot: createProbot(),
-})
+// module.exports.handler = createLambdaFunction(app, {
+//   probot: createProbot(),
+// })
+
+module.exports.webhooks = function(event, context, callback) {
+  console.log('YOOOO!!!')
+  callback(null, {
+      statusCode: 200, 
+      body: JSON.stringify({ hey: 'world' })
+  })
+};
